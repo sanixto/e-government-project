8c670da1d6964dd1f6a262c24063a9f4d13179b3
sanixto <sanix.to@gmail.com>
2021-09-12 13:30:08 +0300

    update left toolbar

--- js/leftToolbar.js
+++ js/leftToolbar.js
@@ -1,18 +1,15 @@
 const toolbar = document.querySelector('.left-toolbar');
 
 document.addEventListener('pointermove', function(event) {
-    const targetElem = event.target;
-    const targetX = targetElem.getBoundingClientRect().left;
+    const targetX = window.event.clientX;
     
-    if (targetX < 5) {
+    if (targetX < 10 && targetX > 0) {
       toolbar.hidden = false;
     }    
-    
-    if (targetX > toolbar.getBoundingClientRect().left) {
-        if (targetElem.closest('.left-toolbar')) return;
-        
-        toolbar.hidden = true;
-    }   
+
+    if (targetX > toolbar.getBoundingClientRect().width) {
+      toolbar.hidden = true; 
+    } 
 })
 
 toolbar.addEventListener('click', function(event) {
